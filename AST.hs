56b472288b93dda1f9e206620a2d64f0e3413e22
Martín Oscar Pavan <pavanmartinoscar@gmail.com>
2026-08-04 21:18:32 -0300

    fix varios AST - inicio ConfigSensores

--- AST.hs
+++ AST.hs
@@ -1,4 +1,8 @@
-data Ubicacion = Cocina | Living | Garage | Pieza | Exterior
+data Ubicacion = Cocina 
+                | Living 
+                | Garage 
+                | Pieza 
+                | Exterior
   deriving (Show, Eq, Ord)
 
 data DiaSemana = Lunes 
@@ -13,20 +17,22 @@ data DiaSemana = Lunes
 data TipoObjeto = TipoEncendible String 
                 | TipoAbrible String 
                 | TipoActivable String 
-                deriving (Show)
+                deriving (Show, Eq, Ord)
 
 -- Objeto general, que deriva de los Tipos de objetos
 data Objeto = Objeto {
   tipo :: TipoObjeto,
   ubicacion :: Ubicacion,
   estado :: EstadoObjeto
-}
+
+} deriving (Show, Eq, Ord)
 
 --Estados para encendibles, abribles y activables
 -- Preguntar si con el nivel solucionamos los estados intermedios y completos
 data EstadoObjeto = EstadoObjeto {
-        nivel :: Int, --de 0 a 100
-}
+        nivel :: Int --de 0 a 100.. 
+                      --0 es apagado, 100 es encendido, 1 a 99 estados intermedios, podemos definir estados intermedios fijos(ej: 25, 50, 75)
+} deriving (Show, Eq, Ord)
 
 
 -- Tipo de sensores definidos
@@ -38,11 +44,22 @@ data EstadoObjeto = EstadoObjeto {
 data TipoSenial = Digital | Analogica
   deriving (Show, Eq)
 
-data Sensor = Sensor {
-    nombreSensor :: String,
-    tipoSenial :: TipoSenial
-  } deriving (Show, Eq)
+type Pin = Int
 
+data Sensor = Sensor {
+    nombreSensor :: String
+  } deriving (Show, Eq, Ord)
+
+type ConfigSensores = [(String, (Pin, TipoSenial))]
+-- Al usuario le tiene que permitir definir el nombre 
+-- del sensor y automaticamente adosarce al pin y tipo de señal correspondiente a ese sensor
+
+--Y si forzamos que al principio de la ejecución del programa, el usuario defina los sensores que va a usar
+-- define Sensores = [Cocina:Temperatura, Living:Luminosidad, Garage:Humedad] 
+--y el programa se encargue de asociarlos a los pines y tipos de señal correspondientes, para que luego el usuario pueda referirse a ellos por nombre.
+
+-- Como definimos que es un sensor digital o analogico?
+-- El usuario sabe diferenciar entre sensor analogico y digital.. 
 
 data Comparador = Mayor | Menor | Igual
   deriving (Show, Eq) -- Ver si lo sacamos??
@@ -70,8 +87,6 @@ data Estado = Estado {
   temperaturas  :: [(Ubicacion, Int)],
   luminosidades :: [(Ubicacion, Int)], 
   humedades ::  [(Ubicacion, Int)],
-  estadosEncendibles  :: [(ObjetoEncendible, EstadoEncendible)],
-  estadosAbribles :: [(ObjetoAbrible, EstadoAbrible)],
-  estadosActivable :: [(ObjetoActivable, EstadoActivable)],
+  estados :: [(Objeto, EstadoObjeto)]
   }
-    deriving Show 
+    deriving Show
